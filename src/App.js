d66ecf456fc9c424387c667a70bea15a115d69e3
layba-azam <laybaazam777@gmail.com>
2023-08-04 14:22:59 +0500

    'first'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const TodoApp = () => {
+  const [todoItem, setTodoItem] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const handleInputChange = (event) => {
+    setTodoItem(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (todoItem.trim() !== '') {
+      setTodoList([...todoList, todoItem]);
+      setTodoItem('');
+    }
+  };
+
+  const deleteItem = (index) => {
+    const updatedList = [...todoList];
+    updatedList.splice(index, 1);
+    setTodoList(updatedList);
+  };
+
+  const editItem = (index) => {
+    const editValue = prompt('Enter Edit Value', todoList[index]);
+    if (editValue !== null) {
+      const updatedList = [...todoList];
+      updatedList[index] = editValue;
+      setTodoList(updatedList);
+    }
+  };
+
+  const deleteAll = () => {
+    setTodoList([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todoo'>
+      <div className="text">
+        <input
+          type="text"
+          name=""
+          id="todo-item"
+          value={todoItem}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTodo} className='btnss'>ADD ITEM</button>
+        <button onClick={deleteAll} className='btnss'>DELETE ALL</button>
+      </div>
+      <div>
+        <ul id="list">
+          {todoList.map((item, index) => (
+            <li key={index} className='list'>
+              {item}
+              <button onClick={() => deleteItem(index)} className='btnss dd'>DELETE</button>
+              <button onClick={() => editItem(index)} className='btnss dd'>EDIT</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
